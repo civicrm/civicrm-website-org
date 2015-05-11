acda128525c9ab7a271e083b0ab1f3ca1471b51a
CiviCRM LLC <info@civicrm.org>
2015-05-11 10:30:40 +0000

    updates to cividownload modules to play nicely with new design

--- drupal/sites/all/modules/cividownload/cividownload.tpl.php
+++ drupal/sites/all/modules/cividownload/cividownload.tpl.php
@@ -1,55 +1,34 @@
-<?php $civicrm_version = variable_get('civicrm_stable_version', '4.5.2'); ?>
-<div class="crm-download-title">CiviCRM <?php echo $civicrm_version; ?></div>
-<div class='crm-support'>
-  <div class='crm-introduction'>
-    The latest stable version of CiviCRM is <?php echo $civicrm_version; ?>. Select a download based on the content management software (CMS) you are using.
-  </div>
-
-  <div class="crm-download-listing">
-    <?php foreach ($content['download_urls'] as $key => $values) { ?>
-      <div class="crm-download-buttons">
-        <a class="download-link" href="<?php echo $values['url'];?>">Download</a>
-        &nbsp;CiviCRM <?php echo $civicrm_version; ?> <?php echo 'for ' . $values['title']; ?></strong>
-      </div>
-      <br/>
-    <?php } ?>
-  </div>
-  <!-- LTS section -->
-
-  <?php $civicrm_lts_version = variable_get('civicrm_lts_version', '4.4.10'); ?>
-<div class="crm-download-title">CiviCRM <?php echo $civicrm_lts_version; ?> LTS</div>
-  <div class="crm-support">
-    <div class="crm-introduction">
-    The current Long Term Support (LTS) release CiviCRM is <?php echo $civicrm_lts_version; ?>. Select a download based on the content management software (CMS) you are using.
-    </div>
-    <div class="crm-download-listing">
-    <?php foreach ($content['download_urls'] as $key => $values) { ?>
-      <div class="crm-download-buttons">
-        <a class="download-link" href="<?php echo $values['url'];?>&rtype=lts">Download</a>
-        &nbsp;CiviCRM <?php echo $civicrm_lts_version; ?> <?php echo 'for ' . $values['title']; ?></strong>
-      </div>
-      <br/>
-    <?php } ?>
-    </div>
-  </div>
-  <!-- Resources section -->
-    <div class="crm=support" style="border: 1px solid grey; padding: 10px;">
-      <div class="crm-introduction" style="font-size: 1.5em;">Resources</div>
-      <div class="crm-introduction" style="font-size: 1.2em;">
-        <a href="https://civicrm.org/versions" target="_blank">Which version should I get? ›</a>
-        <br />
-        <a href="http://sourceforge.net/projects/civicrm/files/" target="_blank">Localization files, previous and pre-release versions (on Sourceforge.net) ›</a>
-        <br />
-        <a href="http://wiki.civicrm.org/confluence/display/CRMDOC/Installation+and+Upgrades" target="_blank">Installation and Upgrade Guides ›</a>
-        <br />
-        <a href="http://book.civicrm.org/user" target="_blank">User and Administrator Guide ›</a>
-        <br />
-        <a href="http://wiki.civicrm.org/confluence/display/CRMDOC/Develop" target="_blank">Developer Documentation ›</a>
-      </div>    
-    </div>
-    <br />
-
-  <!-- Hosting link section -->
-  <div style="background-color: #7EC757; padding: 10px; color: white; font-weight: bold; font-size: 18px; width: 90%'">
-    OR ... <a href="https://civicrm.org/providers/hosting" target="_blank">start using CiviCRM on demand &quot;in the cloud&quot; by signing up with one of our Hosting Providers.</a></div>
+<!-- Stable releases section -->
+<div id="dl-stable">
+	<?php $civicrm_version = variable_get('civicrm_stable_version', '4.5.2'); ?>
+	<div id="dl-stable-top">
+		<img src="/sites/all/themes/civicrm/css/images/download-big.png" width="234" height="230" alt="V" id="dl-arrow" />
+		<h2 class="page-title">The latest stable version of CiviCRM is <?php echo $civicrm_version; ?>.</h2>
+		<p>Select a download based on the content management software (CMS) you are using.</p>
+	</div>
+	<div id="dl-options">
+		<?php foreach ($content['download_urls'] as $key => $values) { ?>
+			<div class="dl-option <?php echo $values['cssclass']; ?>">
+				<img src="/sites/all/themes/civicrm/css/images/cms-<?php echo $values['cms']; ?>.png" width="65" height="65" alt="CMS icon" />
+				<div class="dl-option-inner">
+					<h3><?php echo $values['cms']; ?></h3>
+					<p class="dl-text"><?php echo 'Compatible with ' . $values['title']; ?>
+					<p><a class="btn-dark" href="<?php echo $values['url'];?>">Download</a></p>
+				</div>
+			</div>
+		<?php } ?>
+	</div>
+</div>
+  
+  
+<!-- LTS section -->
+<div id="dl-lts">
+	<?php $civicrm_lts_version = variable_get('civicrm_lts_version', '4.4.10'); ?>
+	<h2 class="page-title">CiviCRM <?php echo $civicrm_lts_version; ?> LTS</h2>
+	<p>The current Long Term Support (LTS) release CiviCRM is <?php echo $civicrm_lts_version; ?>.<br />Select a download based on the content management software (CMS) you are using.</p>
+	<div class="dl-lts-options">
+		<?php foreach ($content['download_urls'] as $key => $values) { ?>
+			<p><strong><a class="dl-link" href="<?php echo $values['url'];?>&rtype=lts"><?php echo 'Compatible with ' . $values['title']; ?></a></strong></p>
+		<?php } ?>
+	</div>
 </div>
