d2dcb4b2801efa84da0c6dc8b0c763d81927f41e
Michael McAndrew <Michael McAndrew>
2015-09-03 22:35:23 -0700

    fixed errors in allowed ckstyles

--- drupal/sites/all/themes/civicrm_theme/ckeditor.styles.js
+++ drupal/sites/all/themes/civicrm_theme/ckeditor.styles.js
@@ -1,21 +1,19 @@
 /*
 Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
-for licensing, see license.html or http://ckeditor.com/license
+For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
 /*
- * this file is used/requested by the 'styles' button.
- * the 'styles' button is not enabled by default in drupalfull and drupalfiltered toolbars.
+ * This file is used/requested by the 'Styles' button.
+ * The 'Styles' button is not enabled by default in DrupalFull and DrupalFiltered toolbars.
  */
-if(typeof(ckeditor) !== 'undefined') {
-    ckeditor.addstylesset( 'drupal',
+if(typeof(CKEDITOR) !== 'undefined') {
+    CKEDITOR.addStylesSet( 'drupal',
     [
-
             { name : 'Heading 2'		, element : 'h2' },
             { name : 'Heading 3'		, element : 'h3' },
             { name : 'Paragraph'		, element : 'p' },
-            { name : 'Quote',			, element : 'p', attributes: { 'class': 'quote'} },
-            { name : 'Code',			, element : 'pre' },
-
+            { name : 'Quote'			, element : 'p', attributes : { 'style' : 'quote' } },
+            { name : 'Code'			, element : 'pre' },
     ]);
 }
