3cf468066ea8935256efa6b2c482221cf5b77eec
Jeremy Proffitt <jeremy@skvare.com>
2015-09-16 17:22:17 -0400

    added white_two region

--- drupal/sites/all/themes/civicrm_theme_omsass/templates/system/page.tpl.php
+++ drupal/sites/all/themes/civicrm_theme_omsass/templates/system/page.tpl.php
@@ -136,6 +136,10 @@ $menu = render($page['menu']);
       <?php print render($page['white']); ?>
       <?php endif; ?>
 
+      <?php if($page['white_two']): ?>
+      <?php print render($page['white_two']); ?>
+      <?php endif; ?>
+
       <?php if($page['blue_two']): ?>
       <?php print render($page['blue_two']); ?>
       <?php endif; ?>
